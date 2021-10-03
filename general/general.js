ac42a9bdf6526e8cbc2d709e808400ef813332f1
Ash Anand <cppanand@gmail.com>
2021-10-03 17:56:26 -0400

    Initial commit with first challenge completed

---
+++ general/general.js
@@ -0,0 +1,41 @@
+// for (var i = 0; i < 5; i++) {
+//   setTimeout(function () {
+//     console.log(i);
+//   }, 1000);
+// }
+
+document.getElementById('table-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const rows = e.target.elements.rows.value;
+  const cols = e.target.elements.cols.value;
+
+  for (let i = 0; i < rows; i++) {
+    const row = document.createElement('tr');
+    for (let j = 0; j < cols; j++) {
+      const col = document.createElement('td');
+      col.textContent = '1'
+      col.setAttribute('id', `${i}-${j}`);
+      row.appendChild(col);
+    }
+    console.log(row)
+    document.querySelector('#table').appendChild(row);
+  }
+
+  let curr = 1;
+  for (let i = 0; i < cols; i++) {
+    if (i % 2 === 0) {
+      for (let j = 0; j < rows; j++) {
+        document.getElementById(`${j}-${i}`).textContent = curr;
+        curr++;
+      }
+    } else {
+      for (let j = rows - 1; j >= 0; j--) {
+        document.getElementById(`${j}-${i}`).textContent = curr;
+        curr++;
+      }
+    }
+  }
+
+
+});
